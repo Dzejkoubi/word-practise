37e5912f21274cf17e9ba2608a881562f6bf2a35
Dzejkoubi <kubakraus06@gmail.com>
2024-01-03 18:41:25 +0100

    basis

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Start />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Game from "./components/Game.vue";
+import Start from "./components/Start.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Start.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>HELLO WORLD</div>
+</template>
+
+<script>
+export default {
+  name: "Start",
+};
+</script>
+
+<style scoped></style>
